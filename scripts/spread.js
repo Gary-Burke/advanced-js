360a9edd67ec81a4ce3a4d6d6b6a83db22d6dd44
gary-burke <garylewisburke@gmail.com>
2025-10-29 10:36:28 +0100

    Add spread script

---
+++ scripts/spread.js
@@ -0,0 +1,33 @@
+// node spread.js
+
+// No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+arr1.push(5);
+console.log("First array: " + arr1);
+console.log("Second array: " + arr2);
+
+// Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+
+arr3.push(7);
+arr4.push(10);
+console.log("First array: " + arr3);
+console.log("Second array: " + arr4);
+
+
+// Copying an object
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { ...obj1, d: 4 };
+let obj3 = { ...obj1, b: 5 };
+
+console.log("First Object:", obj1);
+console.log("Second Object:", obj2);
+console.log("Third Object:", obj3);
+
+
+// Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+console.log("Combined new array:", arr5);
