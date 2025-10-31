fc4338605fb3b24028c1401ca63b57b9ae6233ac
gary-burke <garylewisburke@gmail.com>
2025-10-31 13:29:00 +0100

    Add reduce script

---
+++ scripts/reduce.js
@@ -0,0 +1,54 @@
+
+// Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
+
+
+const teamMembers = [
+  {
+    name: 'Andrew',
+    profession: 'Developer',
+    yrsExperience: 5
+  },
+  {
+    name: 'Ariel',
+    profession: 'Developer',
+    yrsExperience: 7
+  },
+  {
+    name: 'Michael',
+    profession: 'Designer',
+    yrsExperience: 1
+  },
+  {
+    name: 'Kelly',
+    profession: 'Designer',
+    yrsExperience: 3
+  }, 
+    {
+    name: 'Tommy',
+    profession: 'Designer',
+    yrsExperience: 3
+  }
+];
+
+
+// Totaling a specific object property
+let totalExp = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience);
+console.log("totalExp:", totalExp);
+
+
+// Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} <-- this is what we want!
+let expByProf = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience;
+    }
+    return acc;
+}, {} ); // {} is our initial value, it starts with an empty object
+
+console.log(expByProf);
